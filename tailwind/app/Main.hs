756a1d6e863581847186f8251c61dfddd88f08a3
Touka Ri <ritouka90@gmail.com>
2021-12-05 14:09:11 +0900

    Fetch chart data according to sync cursor

--- tailwind/app/Main.hs
+++ tailwind/app/Main.hs
@@ -2,6 +2,7 @@
 
 module Main (main) where
 
+import Data.Time.Clock
 import Database.PostgreSQL.Simple
 import Protolude
 import Tailwind.DataSource.CoinGecko
@@ -10,12 +11,18 @@ import Tailwind.DB.Queries
 db :: ByteString
 db = "postgres://postgres:password@localhost:5432/voyager?sslmode=disable"
 
+interval :: NominalDiffTime
+interval = nominalDay
+
 main :: IO ()
 main = do
   conn <- connectPostgreSQL db
   t <- readSyncCursor conn "solana"
   putText $ "sync_cursor t=" <> show t
-  ts <- fetchTickers "solana"
+  let start = fromMaybe undefined t
+  let end = addUTCTime interval start
+  putText $ "next_range start=" <> show start <> " end=" <> show end
+  ts <- fetchTickers "solana" (start, end)
   putText $ "fetch_data count=" <> show (length ts)
   count <- insertTickers conn "solana" ts
   putText $ "write_db count=" <> show count
--- tailwind/src/Tailwind/DataSource/CoinGecko.hs
+++ tailwind/src/Tailwind/DataSource/CoinGecko.hs
@@ -3,6 +3,8 @@
 module Tailwind.DataSource.CoinGecko (fetchTickers) where
 
 import Data.Text
+import Data.Time
+import Data.Time.Clock.POSIX
 import qualified Network.Wreq as W
 import qualified Network.Wreq.Optics as WO
 import Optics
@@ -13,13 +15,16 @@ import Tailwind.Types (Ticker)
 chartURL :: Text -> Text
 chartURL id = "https://www.coingecko.com/price_charts/" <> id <> "/jpy/custom.json"
 
-fetchTickers :: Text -> IO [Ticker]
-fetchTickers id = do
+toTimestamp :: UTCTime -> Text
+toTimestamp = show @Int . truncate . utcTimeToPOSIXSeconds
+
+fetchTickers :: Text -> (UTCTime, UTCTime) -> IO [Ticker]
+fetchTickers id (start, end) = do
   let url = unpack $ chartURL id
   resp <- W.asValue =<< W.getWith opts url
   let tickers = parseChart $ resp ^. WO.responseBody
   maybe undefined pure tickers
   where
     opts = W.defaults
-      & WO.param "from" .~ ["1586444400"]
-      & WO.param "to" .~ ["1586530800"]
+      & WO.param "from" .~ [toTimestamp start]
+      & WO.param "to" .~ [toTimestamp end]
